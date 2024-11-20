8731691cbf95f2c5c2d8e25aa30b479c774f0b2b
daramgom <daramgom@outlook.com>
2024-11-21 00:00:47 +0900

    입양VO클래스-생성

---
+++ Unicon_r1/src/main/java/com/Unicon/domain/AdptMemberVO.java
@@ -0,0 +1,14 @@
+package com.Unicon.domain;
+
+import lombok.Data;
+
+@Data
+public class AdptMemberVO {
+	private String member_id;
+	private String member_name;
+	private String member_tel;
+	private String member_addr;
+	private String member_email;
+	private int membar_age;
+	private String member_gender;
+}
