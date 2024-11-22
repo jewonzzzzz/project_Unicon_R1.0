8330a95bb305c75013ca82bcfd7f4905b3c94512
daramgom <daramgom@outlook.com>
2024-11-22 20:38:29 +0900

    Transactional테스트확인

--- Unicon_r1/src/main/java/com/Unicon/service/AdptService.java
+++ Unicon_r1/src/main/java/com/Unicon/service/AdptService.java
@@ -1,8 +1,12 @@
 package com.Unicon.service;
 
+import java.sql.SQLException;
+
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.Unicon.domain.AdptVO;
 import com.Unicon.persistence.AdptDAO;
@@ -16,6 +20,7 @@ public class AdptService {
 	@Inject
 	AdptDAO adptDAO;
 	
+	@Transactional(rollbackFor = {SQLException.class, Exception.class}, propagation = Propagation.REQUIRES_NEW)
 	public void adptInsert(AdptVO adptVO) {
 		log.info("( •̀ ω •́ )✧ adptInsert() 실행");
 		adptDAO.adptInsert(adptVO);
