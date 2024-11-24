b04fdecc91b2ba1a3d82c5b9a0082d196fb212c0
zmffk <zmffk@DESKTOP-A253FQG>
2024-11-25 00:36:43 +0900

    커뮤니티 게시물 등록 기능

--- Unicon_r1/src/main/java/com/Unicon/persistence/CommunityDAO.java
+++ Unicon_r1/src/main/java/com/Unicon/persistence/CommunityDAO.java
@@ -3,7 +3,12 @@ package com.Unicon.persistence;
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+
+import com.Unicon.domain.ImageVO;
+import com.Unicon.domain.PostVO;
 
 @Repository("CommunityDAO")
 public class CommunityDAO {
@@ -12,5 +17,18 @@ public class CommunityDAO {
 	private SqlSession sqlSession;
 	
 	private static final String NAMESPACE = "com.Unicon.mapper.CommunityMapper.";
+	
+	
+	private static final Logger logger = LoggerFactory.getLogger(CommunityDAO.class);
+	
+	// 게시물 등록
+	public void postInsert(PostVO postVO) {
+		logger.info(" DAO - postInsert() 실행 ");
+		
+		sqlSession.insert(NAMESPACE+"postInsert", postVO);
+		for(ImageVO imageVO : postVO.getPost_images()) {
+			sqlSession.insert(NAMESPACE+"postImageInsert", imageVO);
+		}
+	} // postInsert()
 	
 }
